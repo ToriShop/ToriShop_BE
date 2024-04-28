7a2aaf83aabd50d105f7dddcfe9905a786026b2a
doSeung11 <dounts706@naver.com>
2024-04-29 01:27:32 +0900

    first commit

---
+++ src/main/java/com/torishop/TorishopApplication.java
@@ -0,0 +1,13 @@
+package com.torishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorishopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorishopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/torishop/TorishopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorishopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
